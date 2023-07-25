4dd9f3d8fdf4b377defab07265c3e5078729afd2
TaeYoungHum <gsdj1990123@naver.com>
2023-07-25 15:26:00 +0900

    1

--- hellocook/src/main/java/com/cook/controller/RecipeCvl.java
+++ hellocook/src/main/java/com/cook/controller/RecipeCvl.java
@@ -129,7 +129,7 @@ public class RecipeCvl implements RecipeController {
 	GoogleTtsService googleTtsService;
 
 	@Override
-	@GetMapping("/main")
+	@GetMapping("/main1")
 	public String viewMain(Model model) throws Exception {
 		// TODO Auto-generated method stub
 		
